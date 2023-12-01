bc42497b2588213ac342000f82f0c97b24e881de
PixelDG <35879142+leoamaro01@users.noreply.github.com>
2023-11-30 19:40:41 -0500

    TeamMemberDto no longer has a TeamDto field, but a teamId field.

--- Data/DTO/TeamMemberDto.cs
+++ Data/DTO/TeamMemberDto.cs
@@ -3,7 +3,7 @@ namespace Data.DTO;
 public class TeamMemberDto
 {
     public int Id { get; set; }
-    public TeamDto Team { get; set; }
+    public int TeamId { get; set; }
     public AthleteDto Athlete { get; set; }
     public string Role { get; set; }
 }
