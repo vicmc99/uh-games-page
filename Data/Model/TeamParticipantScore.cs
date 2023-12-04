ee9d11cb783ea4ef7572c302a577568ce53cb23c
PixelDG <35879142+leoamaro01@users.noreply.github.com>
2023-12-04 01:07:23 -0500

    Reapplied fixes

--- Data/Model/TeamParticipantScore.cs
+++ Data/Model/TeamParticipantScore.cs
@@ -3,8 +3,9 @@ namespace Data.Model;
 public class TeamParticipantScore
 {
     public int EventId { get; set; }
-    [Key] public int TeamId { get; set; }
-
+    public ParticipantScoredEvent Event { get; set; }
+    public int TeamId { get; set; }
+    public NormalTeam Team { get; set; }
     public int ParticipantId { get; set; }
     public TeamMember Participant { get; set; }
     public int ScoreId { get; set; }
